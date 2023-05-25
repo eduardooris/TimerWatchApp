6effe1f1c07c77daeb31dae45c9d0c327841147e
eduardooris <orisdevgg@gmail.com>
2023-05-25 16:14:05 -0300

    i

---
+++ TimerWatchApp/TimerWatchApp Watch App/CountdownView.swift
@@ -0,0 +1,20 @@
+//
+//  CountdownView.swift
+//  TimerWatchApp Watch App
+//
+//  Created by tecnorise desenvolvimento on 25/05/23.
+//
+
+import SwiftUI
+
+struct CountdownView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CountdownView_Previews: PreviewProvider {
+    static var previews: some View {
+        CountdownView()
+    }
+}
---
+++ TimerWatchApp/TimerWatchApp Watch App/TimerWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerWatchAppApp.swift
+//  TimerWatchApp Watch App
+//
+//  Created by tecnorise desenvolvimento on 25/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TimerWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
